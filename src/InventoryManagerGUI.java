5c22a137de7b48b544b95b93678d3fd718dee680
peter <peter@LAPTOP-C7425T5R>
2024-07-11 13:48:59 -0500

    Correction of revision to display the revised GUI

--- src/InventoryManagerGUI.java
+++ src/InventoryManagerGUI.java
@@ -4,136 +4,114 @@
  */
 import java.io.File;
 import javax.swing.*;
-
-import java.awt.BorderLayout;
+import java.awt.*;
 import java.awt.event.*;
 
 
-public class InventoryManagerGUI {
+public class InventoryManagerGUI implements ActionListener {
 
 	public static JFrame managerFrame;
 	private JMenuBar managerMenuBar;
-	private JMenu databaseMenu, inventoryMenu, helpMenu;
-	private JMenuItem about, dbItem_1, dbItem_2, inventoryItem_1, inventoryItem_2;
-	private JPanel activePanel;
-	private JTextField usrnameField, pswdField;
-	
+	private JMenu database, inventory, help;
+	private JMenuItem about, configure, reset, imporT, update;
+	private JPanel activePanel = new JPanel();
+
 		
 	InventoryManagerGUI ()
 	{
-		// Menu items
-		about = new JMenuItem("About");
-		dbItem_1 = new JMenuItem("Configure");
-		dbItem_2 = new JMenuItem("Reset");
-		inventoryItem_1 = new JMenuItem("Import");
-		inventoryItem_2 = new JMenuItem("Update");
-		
-		// Menus
-		databaseMenu = new JMenu("Database");
-		inventoryMenu = new JMenu("Inventory");
-		helpMenu = new JMenu("Help");
-		
-		// Menu bar
-		managerMenuBar = new JMenuBar();
-		
-		
 		/*
 		 *  Laying out the manager window frame
 		 */
 		managerFrame = new JFrame("MESECT Online Store Inventory Management Utility");
 		managerFrame.setSize(600,400);
 		
-		// Add the menu items to their respective menus
 		
-
+		// Menu items
+		about = new JMenuItem("About");
+		configure = new JMenuItem("Configure");
+		reset = new JMenuItem("Reset");
+		imporT = new JMenuItem("Import");
+		update = new JMenuItem("Update");
+		
+		// Menus
+		database = new JMenu("Database");
+		inventory = new JMenu("Inventory");
+		help = new JMenu("Help");
+		
+		// Menu bar
+		managerMenuBar = new JMenuBar();
+		
+		
+		// *** Add the menu items to their respective menus ***
+		
 		// About menu
-		helpMenu.add(about);
+		help.add(about);
 		
 		// Database menu items
-		databaseMenu.add(dbItem_1);
-		databaseMenu.add(dbItem_2);
+		database.add(configure);
+		database.add(reset);
 		
 		// Inventory menu items
-		inventoryMenu.add(inventoryItem_1);
-		inventoryMenu.add(inventoryItem_2);
+		inventory.add(imporT);
+		inventory.add(update);
 		
 		// Add the menus to the menubar
-		managerMenuBar.add(databaseMenu);
-		managerMenuBar.add(inventoryMenu);
-		managerMenuBar.add(helpMenu);
+		managerMenuBar.add(database);
+		managerMenuBar.add(inventory);
+		managerMenuBar.add(help);
 		
 		// Add the menu bar to the window frame
-		managerFrame.add(managerMenuBar, BorderLayout.NORTH);
-		managerFrame.setVisible(true);
+		managerFrame.add(managerMenuBar, BorderLayout.NORTH);		
 		
-		
-		/*
-		 * Add and configure event listener
-		 */
+		// Add and configure action listener to manager window
 		managerFrame.addWindowListener(new WindowAdapter(){
 			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
-			}
-		});
+			}			
+		});		
 		
+		// Add action listener to menu selections
+		configure.addActionListener(this);
+		reset.addActionListener(this);
+		imporT.addActionListener(this);
+		update.addActionListener(this);
+		about.addActionListener(this);
 		
-		
-		
-		/*
-		 * Configuring the connect button
-		 * to connect with database when it is clicked
-		 */
-//		dbConnectButton = new Button("Connect");
-//		dbConnectButton.setActionCommand("Connect");
-//		dbConnectButton.addActionListener(new ButtonClicked());
-		
-		
-	// ********* END OF CONSTRUCTOR ***********
+		managerFrame.setVisible(true);
 	}
 	
-	/*
-	 * Set the database connection password
-	 * to the user supplied password
-	 */
-	private void setPassword(String password) {
-		Product.currentPassword = password;
-	}
-	
-	/*
-	 * Set the user name for the database connection
-	 * to the user supplied user name
-	 */
-	private void setUserName(String usrName) {
-		Product.userName = usrName;
-	}
-	
-	
-	
-	private class ButtonClicked implements ActionListener {
-		
+	// Execute actions subject to menu selection 
+		@Override
 		public void actionPerformed(ActionEvent e) {
-			String cmd = e.getActionCommand();
-			if(cmd.equals( "Connect" )) {
-				setPassword(pswdField.getText());
-				setUserName(usrnameField.getText());				
-				File sourceFile = new File("C:\\Users\\peter_000\\"
-						+ "Downloads\\Laserprinters.htm");
-				IntcomexInventory inventory;
-				try {
-					inventory = new IntcomexInventory(sourceFile);
-					inventory.getProducts();
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-					
-				}
-								
-				managerFrame.dispose();
+			
+			if( e.getSource() == configure)
+			{
+				
 			}
+			
+			if( e.getSource() == configure)
+			{
+				
+			}
+			
+			if( e.getSource() == reset)
+			{
+				
+			}
+			
+			if( e.getSource() == imporT)
+			{
+				
+			}
+			
+			if( e.getSource() == about)
+			{
+				
+			}
+		
+			
 		}
 		
 		
-		
-	}
 }
